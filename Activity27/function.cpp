26f33563591a7b6521ff0e5c631819c44721b887
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-11-04 18:03:20 -0800

    Completed Program with 3 files: main, header, and function files. Program has 2 structures, has 2 functions to find and add a book, has a operator overloading (<<).

---
+++ Activity27/function.cpp
@@ -0,0 +1,8 @@
+//
+//  function.cpp
+//  Activity27
+//
+//  Created by Chatchapong Thuaypha on 11/3/24.
+//
+
+#include <stdio.h>
---
+++ Activity27/header.h
@@ -0,0 +1,12 @@
+//
+//  header.h
+//  Activity27
+//
+//  Created by Chatchapong Thuaypha on 11/3/24.
+//
+
+#ifndef header_h
+#define header_h
+
+
+#endif /* header_h */
